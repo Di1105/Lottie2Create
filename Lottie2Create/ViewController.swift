66b3009f936f20f3049c7bc982ee58540b84102d
Di1105 <delcioglu@gmail.com.com>
2022-12-23 10:33:09 +0300

    First Commit

--- Lottie2Create/ViewController.swift
+++ Lottie2Create/ViewController.swift
@@ -6,12 +6,73 @@
 //
 
 import UIKit
+import SnapKit
+import Lottie
 
 class ViewController: UIViewController {
+    
+    private var lottieAnimation = LottieAnimationView()
+    let labelText = UILabel()
+   // let counterLabel = UILabel()
+    var counter = 0
+    var timer = Timer()
+    var lottieA = LottieAnimationView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        setUpUI()
+        timerLabel()
+
+    }
+    
+    func setUpUI(){
+        view.addSubview(lottieA)
+        view.addSubview(labelText)
+        //view.addSubview(counterLabel)
+        
+        lottieA.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.height.width.equalTo(100)
+        }
+        lottieA.animation = LottieAnimation.named("wait")
+        lottieA.contentMode = .scaleAspectFit
+        lottieA.loopMode = .loop
+        lottieA.play()
+        
+        labelText.snp.makeConstraints { make in
+            make.top.equalTo(lottieA.snp.bottom).offset(30)
+            make.centerXWithinMargins.equalToSuperview()
+            make.height.equalTo(20)
+            //make.width.equalTo(100)
+        }
+        /*
+        counterLabel.snp.makeConstraints { make in
+            make.top.equalTo(labelText.snp.bottom).offset(30)
+            make.centerXWithinMargins.equalToSuperview()
+            make.height.equalTo(20)
+            //make.width.equalTo(100)
+        }
+        */
+    }
+    
+     func timerLabel(){
+
+          timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(printInfo), userInfo: nil, repeats: true)
+    }
+    
+    func animateLoadLottie(){
+        
+    }
+    
+    @objc func printInfo(){
+        //counterLabel.text = String(counter)
+        counter = counter + 1
+                if counter == 7 {
+                    timer.invalidate()
+                    labelText.text = "Anasayfaya yönlendiriliyorsunuz"
+                }
+        
     }
 
 
